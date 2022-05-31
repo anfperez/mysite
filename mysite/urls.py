dcf7ca1b4607ecb0383e0583af4309716fd728fb
Angela Perez <pereza@dnc.org>
2022-05-31 14:47:32 -0400

    first commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 #from django.conf.urls import url
 from django.contrib import admin
-from django.urls import include, re_path
+from django.urls import include, path
 
 urlpatterns = [
-    re_path(r'^admin/', admin.site.urls),
-    re_path('polls/', include('polls.urls'))
+    path(r'^admin/', admin.site.urls),
+    path('polls/', include('polls.urls'))
 ]
